1c245fcc6a181fd5da00d1283405467ed7987b09
tube(한승윤) <127028297+aberica@users.noreply.github.com>
2025-06-30 23:01:42 +0900

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -3,15 +3,27 @@
 #include <queue>
 #include <vector>
 #include <algorithm>
-#include <unordered_set>
+#include <unordered_map>
 #define STORE_MAX 100'001
 #define INF 2'100'000'000
 using namespace std;
 
+struct Dists{
+	int d[3], idx;
+	Dists(int _a=INF, int _b=INF, int _c=INF, int _idx = -1) {
+		d[0] = _a;
+		d[1] = _b;
+		d[2] = _c;
+		idx = _idx;
+	}
+};
+
 int city_num, apartments[3];
 vector<pair<int, int>> roads[STORE_MAX];
-int dists[3][STORE_MAX];
-int city_idxs[STORE_MAX], city_orders[STORE_MAX];
+Dists dists[STORE_MAX];
+bool affordable[STORE_MAX];
+int seg_tree[STORE_MAX * 4];
+int sorted_idx[STORE_MAX];
 
 void Init() {
 	cin >> city_num;
@@ -24,141 +36,87 @@ void Init() {
 	}
 }
 void CalDist() {
+	fill(dists, dists + STORE_MAX, Dists(INF, INF, INF));
+	for (int i = 1; i <= city_num; i++) dists[i].idx = i;
 	for (int i = 0; i < 3; i++) {
-		fill(dists[i], dists[i] + STORE_MAX, INF);
-		dists[i][apartments[i]] = 0;
+		dists[apartments[i]].d[i] = 0;
 		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
 		pq.push({0, apartments[i]});
 		while(!pq.empty()) {
 			auto u = pq.top();
 			pq.pop();
-			if (u.first > dists[i][u.second]) continue;
+			if (u.first > dists[u.second].d[i]) continue;
 			for (auto road : roads[u.second]) {
 				pair<int, int> v = {road.first + u.first, road.second};
-				if (v.first >= dists[i][v.second]) continue;
-				dists[i][v.second] = v.first;
+				if (v.first >= dists[v.second].d[i]) continue;
+				dists[v.second].d[i] = v.first;
 				pq.push(v);
 			}
 		}
 	}
 }
-
 void SortDist() {
-	auto cmp = [](const int& a, const int& b) {
-		if (dists[0][a] != dists[0][b]) return dists[0][a] < dists[0][b];
-		else if (dists[1][a] != dists[1][b]) return dists[1][a] < dists[1][b];
-		else return dists[2][a] < dists[2][b];
+	auto cmp = [](const Dists a, const Dists b) {
+		if (a.d[0] != b.d[0]) return a.d[0] < b.d[0];
+		else return a.d[1] > b.d[1];
 	};
-	for (int i = 1; i <= city_num; i++) city_idxs[i] = i;		// [1st] -> city_idx
-	sort(city_idxs + 1, city_idxs + 1 + city_num, cmp);
-	for (int i = 1; i <= city_num; i++) city_orders[city_idxs[i]] = i;	// [city_idx] -> 1st
+	sort(dists + 1, dists + 1 + city_num, cmp);
+	for (int i = 1; i <= city_num; i++) sorted_idx[dists[i].idx] = i;
+	// for (int i = 1; i <= city_num; i++) city_idxs[i] = i;		// [1st] -> city_idx
+	// sort(city_idxs + 1, city_idxs + 1 + city_num, cmp);
+	// for (int i = 1; i <= city_num; i++) city_orders[city_idxs[i]] = i;	// [city_idx] -> 1st
 }
+int query(int node, int start, int end, int left, int right) {
+	if (right < start || end < left) return INF;
+	if (left <= start && end <= right) {
+		return seg_tree[node];
+	}
+	int mid = (start + end) / 2;
+	return min(query(node * 2, start, mid, left, right), query(node * 2 + 1, mid + 1, end, left, right));
+}
+void update(int node, int start, int end, int val, int pos) {
+	if (pos < start || end < pos) return;
+	if (start == end) {
+		seg_tree[node] = min(seg_tree[node], val);
+		return;
+	}
+	int mid = (start + end) / 2;
+	update(node * 2, start, mid, val, pos);
+	update(node * 2, mid + 1, end, val, pos);
+	seg_tree[node] = min(seg_tree[node * 2], seg_tree[node * 2 + 1]);
+}
+void CalAfford() {
+	vector<int> b_values;
+	for (int i = 1; i <= city_num; i++) b_values.push_back(dists[i].d[1]);
+	sort(b_values.begin(), b_values.end());
+	b_values.erase(unique(b_values.begin(), b_values.end()), b_values.end());
 
+	unordered_map<int, int> b_comp;
+	for (int i = 0; i < (int)b_values.size(); i++) b_comp[b_values[i]] = i + 1;
+
+	fill(seg_tree, seg_tree + STORE_MAX * 4, INF);
+	fill(affordable, affordable + STORE_MAX, false);
+	for (int i = 1; i <= city_num; i++) {
+		int cur_b = b_comp[dists[i].d[1]];
+		int min_val = query(1, 1, city_num, 1, cur_b - 1);  // B' < B
+		if (min_val < dists[i].d[2]) affordable[i] = false;
+		else affordable[i] = true;
+		update(1, 1, city_num, dists[i].d[2] + 1, cur_b);  // z < c 조건 유도
+	}
+}
 
 int main() {
 	Init();
 	CalDist();
 	SortDist();
+	CalAfford();
 
 	int query_num; cin >> query_num;
 	for (auto q = 0; q < query_num; q++) {
 		int idx; cin >> idx;
-
-		bool dominated = false;
-		for (int i = 1; i < city_orders[idx]; i++) {
-			int prior_idx = city_idxs[i];
-			if (dists[0][prior_idx] < dists[0][idx] &&
-				dists[1][prior_idx] < dists[1][idx] &&
-				dists[2][prior_idx] < dists[2][idx]) {
-				dominated = true;
-				break;
-			}
-		}
-		if (dominated) printf("NO\n");
-		else printf("YES\n");
+		
+		if (affordable[sorted_idx[idx]]) printf("YES\n");
+		else printf("NO\n");
 	}
 	return 0;
 }
-
-
-'''
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map>
-using namespace std;
-
-const int INF = 2'000'000'000;
-
-struct Child {
-    int a, b, c;
-    bool operator<(const Child& other) const {
-        if (a != other.a) return a < other.a;
-        return b > other.b; // a가 같을 경우 b 큰 게 먼저!
-    }
-};
-
-vector<int> segTree;
-
-void update(int node, int start, int end, int index, int value) {
-    if (index < start || index > end) return;
-    if (start == end) {
-        segTree[node] = min(segTree[node], value);
-        return;
-    }
-    int mid = (start + end) / 2;
-    update(node * 2, start, mid, index, value);
-    update(node * 2 + 1, mid + 1, end, index, value);
-    segTree[node] = min(segTree[node * 2], segTree[node * 2 + 1]);
-}
-
-int query(int node, int start, int end, int l, int r) {
-    if (r < start || end < l) return INF;
-    if (l <= start && end <= r) return segTree[node];
-    int mid = (start + end) / 2;
-    return min(
-        query(node * 2, start, mid, l, r),
-        query(node * 2 + 1, mid + 1, end, l, r)
-    );
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<Child> kids(n);
-    vector<int> b_values;
-
-    for (int i = 0; i < n; ++i) {
-        cin >> kids[i].a >> kids[i].b >> kids[i].c;
-        b_values.push_back(kids[i].b);
-    }
-
-    // 1. 좌표 압축
-    sort(b_values.begin(), b_values.end());
-    b_values.erase(unique(b_values.begin(), b_values.end()), b_values.end());
-    map<int, int> b_index;
-    for (int i = 0; i < b_values.size(); ++i)
-        b_index[b_values[i]] = i;
-
-    int M = b_values.size();
-    segTree.assign(4 * M, INF); // 초기화
-
-    // 2. a 기준 정렬, a 같으면 b 큰 것부터
-    sort(kids.begin(), kids.end());
-
-    int installable = 0;
-    for (auto& kid : kids) {
-        int b_idx = b_index[kid.b];
-        // 3. b < 현재 b인 애들 중 최소 c 찾기
-        int min_c = query(1, 0, M - 1, 0, b_idx - 1);
-        if (min_c > kid.c) {
-            installable++; // 자신보다 완전 작은 애가 없음
-        }
-        // 4. 현재 아이 정보를 세그 트리에 업데이트
-        update(1, 0, M - 1, b_idx, kid.c);
-    }
-
-    cout << installable << '\n';
-    return 0;
-}
-'''
